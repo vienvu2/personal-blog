66fe2598abe6808a4b00bf060424505f524b9cfb
vienvu <vien@vienvu.com>
2021-03-21 10:57:17 +0700

    1

--- components/Header.vue
+++ components/Header.vue
@@ -11,6 +11,7 @@
         >
           Trang chủ
         </router-link>
+        <div class="flex-1" />
         <router-link
           class="p-3 inline-block cursor-pointer hover:text-green-500 border-b-2 border-transparent hover:border-green-500 "
           to="/showroom"
@@ -20,23 +21,23 @@
         <div class="flex flex-col group relative">
           <router-link
             class="p-3 inline-block cursor-pointer hover:text-green-500 border-b-2 border-transparent hover:border-green-500 "
-            to="/hobby"
+            to="/blog"
           >
-            Sở thích
+            Blog
             <i class="fa fa-chevron-down" />
           </router-link>
           <div class="absolute top-0 left-0 flex-col hidden group-hover:flex">
-            <router-link class="p-2 block" to="/hobby/programming">
+            <router-link class="p-2 block" to="/blog/programming">
               Lập trình
             </router-link>
-            <router-link class="p-2" to="/hobby/travel"> Du lịch </router-link>
-            <router-link class="p-2" to="/hobby/movie">
+            <router-link class="p-2" to="/blog/travel"> Du lịch </router-link>
+            <router-link class="p-2" to="/blog/movie">
               Đánh giá phim
             </router-link>
-            <router-link class="p-2" to="/hobby/book">
+            <router-link class="p-2" to="/blog/book">
               Đánh giá sách
             </router-link>
-            <router-link class="p-2" to="/hobby/game">
+            <router-link class="p-2" to="/blog/game">
               Đánh giá game
             </router-link>
           </div>
@@ -47,9 +48,8 @@
         >
           Tôi là ai?
         </router-link>
-        <div class="flex-1" />
-        <div class="p-1">
-          <i class="fa fa-search" />
+        <div class="m-1 relative">
+          <i class="fa fa-search absolute top-0 left-0" />
           <input
             class="p-2 border border-green-500 focus:border-green-700"
             @keyup.enter="search"
