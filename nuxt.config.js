0e745656a642a2d9cdfe06205cab4b4bb2683561
vienvu <vien@vienvu.com>
2021-06-03 22:22:12 +0700

    1

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "server",
-
+  generate: {
+    dir: 'public'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "Page",
